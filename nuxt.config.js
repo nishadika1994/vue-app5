d944eedef56bc4a5d5a66f30a079c75479fba899
NipuniWickramasinghe <81399550+NipuniWickramasinghe@users.noreply.github.com>
2022-07-12 08:16:12 +0530

    add scss changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/scss/style1.scss',
+    '~/assets/scss/mixins.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,22 @@
-<template>
-    <div>
+<template class = "td">
+    <div class="div1">
         <h1>about page</h1>
     </div>
 </template>
+<style lang='scss'>
+@import '~assets/scss/style1';
+@import '~assets/scss/mixins';
+h1{
+    color: $orange;
+}
+
+.div1{
+    background: burlywood;
+    @include my-mixin-name
+}
+
+.td{
+
+    background: blue;
+}
+</style>
